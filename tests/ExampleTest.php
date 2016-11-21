1fbd8ffa0c28ed33792e0365f82febb1c04a3764
taloto07 <taloto07@gmail.com>
2016-11-20 23:01:42 -0800

    push everything needed for this project

---
+++ tests/ExampleTest.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic functional test example.
+     *
+     * @return void
+     */
+    public function testBasicExample()
+    {
+        $this->visit('/')
+             ->see('Laravel');
+
+        $this->seeInDatabase('users', [
+                'email' => 'taloto07@yahoo.com'
+            ]);
+    }
+}
